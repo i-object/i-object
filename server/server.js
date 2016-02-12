565c4dc16ca7c558c57056997010122746900aac
Eric Mustin <mustin.eric@gmail.com>
2016-02-11 19:49:35 -0500

    [api] added api call and increased scope of schema

--- server/server.js
+++ server/server.js
@@ -3,27 +3,18 @@ var partials = require('express-partials');
 var bodyParser = require('body-parser');
 var http = require('http');
 
-
 var app = express();
-
 var router = express.Router();
 
 app.use(partials());
 app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded( { extended: true }));
+app.use(bodyParser.urlencoded( { extended: true }));
 
 app.use(express.static(__dirname+'/../public/client'));
 
 app.use('/api', router);
-
 require('./router.js')(router);
-
-// function (router)
-
-
 
 app.set('port', (process.env.PORT || 8000) );
 
 module.exports = app;
-
-
