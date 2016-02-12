3afc0ff8003c15f30e9859bcbda494b39e033295
Eric Mustin <mustin.eric@gmail.com>
2016-02-11 19:49:35 -0500

    [api] added api call and increased scope of schema

--- db/model.js
+++ db/model.js
@@ -4,7 +4,10 @@ var mongoose = require('mongoose');
 
 var occurSchema = new mongoose.Schema( {
   date: Date,
-  user: String
+  user: String,
+  weather: String,
+  temperature: Number,
+  pressure: Number
 } );
 
 var Occur = mongoose.model('Occur', occurSchema);
--- server/controller.js
+++ server/controller.js
@@ -1,41 +1,55 @@
 var Occur = require('../db/model.js');
+var request = require('request');
 
 var controller = {};
 
 controller.sendButtonData = function(req,res) {
-  console.log("this request body for post in controller looks like: ", req.body)
   var currentDate = Date.now()
-  //this is going to depend on what the client side req body looks like
-  var buttonData = new Occur({ user: req.body.user, date: currentDate});
 
-  buttonData.save(function(err, newButtonData) {
-    console.log('hey we got here');
-    if(err) {
-      res.send(500, err);
-    } else {
-      console.log('hey the save worked for: ', newButtonData);
-      res.send(200,newButtonData);
-    }
-  });
+  var latitude = req.body.latitude;
+  var longitude = req.body.longitude;
+  var apiKey = 'a596d9028162d68b23321b5fd78bbe1d';
+
+    var darkSky = function(lat,lon,apiKey) {
+      var url = 'https://api.forecast.io/forecast/'+apiKey+'/'+lat+","+lon;
+      
+      request.get(url, function(err,response,body){
+        var skyBody = JSON.parse(body);
+        var currentSky = skyBody.currently;
+
+        //this is going to depend on what the client side req body looks like
+        var buttonData = new Occur({user: req.body.user,
+                                    date: currentDate, 
+                                    weather: currentSky.summary,
+                                    temperature: currentSky.temperature,
+                                    pressure: currentSky.pressure
+                                  });
+
+        buttonData.save(function(err, newButtonData) {
+          if(err) {
+            console.log('we got an error in save');
+            res.send(500, err);
+          } else {
+            console.log('hey the save worked for: ', newButtonData);
+            res.send(200,newButtonData);
+          }
+        });
+      });
+    };  
+
+  darkSky(latitude,longitude,apiKey);  
 };
 
-
 controller.retrieveStats = function(req,res) {
-  console.log("this request body for get in controller looks like: ", req.body)
-
   Occur.find({user:req.body.user}).exec(function(err,found) {
     if(err) {
+      console.log('we had an error retrieving stats');
       res.send(404, err);
     } else {
       console.log("The data we retrieved looks like: ", found);
       res.send(200, found);
     }     
   });
-
-
-
-
-
 };
 
 
